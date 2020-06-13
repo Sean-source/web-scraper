58dc7529c7c7292e5db118f1f88ea4e0f17ffc87
Sean Baklini <sbaklini@unm.edu>
2020-06-13 08:27:36 -0600

    Main Structure of web scraping working

--- controller/index.js
+++ controller/index.js
@@ -3,12 +3,52 @@ var router = express();
 var cheerio = require("cheerio");
 var db = require("../models");
 var axios = require("axios");
-router.get("/scrape", function(req, res) {
-      axios.get("https://www.nytimes.com/").then(function(results) {
-          var $ = cheerio.load(results.data)
 
-          $("")
-      })
+
+router.get("/", function(req, res) {
+    db.Article.find({saved:false}).then(function(results) {
+        var newResults = results.map(news => {
+            return {
+                headline: news.headline,
+                summary: news.summary,
+                URL: news.URL,
+                image: news.image,
+                id: news._id
+
+            }
+        })
+        res.render("index", {news: newResults})
+    })
+})
+
+
+router.get("/scrape", function (req, res) {
+    db.Article.remove({}).then (function(results) {
+        axios.get("https://stocknews.com/top-stories").then(function (results) {
+            var $ = cheerio.load(results.data)
+    
+            $("div.margin-bottom").each(function (i, element) {
+                var title = $(this).children("div.margin-top-mobile").children("div.pad-left").children("h3.pad-top").children("a").text();
+                var link = $(this).children("div.margin-top-mobile").children("div.pad-left").children("h3.pad-top").children("a").attr("href");
+                var summary = $(this).children("div.margin-top-mobile").children("div.pad-left").text();
+                var image = $(this).children("div.no-mobile-pull").children("a").children("img").attr("data-src");
+    
+                console.log(title, link, summary, image)
+                db.Article.create({
+                    headline: title,
+                    summary: summary.replace(title, "")  ,
+                    URL: "https://stocknews.com" + link,
+                    image: image
+                })
+    
+    
+            })
+    
+            res.send("scraping is completed");
+        })
+
+    })
+  
 })
 
 module.exports = router;
--- models/articles.js
+++ models/articles.js
@@ -6,6 +6,7 @@ var articleSchema = new Schema({
     summary: {type: String, required: true},
     URL: {type: String, required: true},
     image: {type: String, required: true},
+    saved:{type:Boolean, default:false},
     note:[{
         type:Schema.Types.ObjectId, 
         ref: "Note"
